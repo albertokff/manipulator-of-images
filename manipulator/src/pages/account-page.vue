c552c436a4a93ad662555be76ea4e160b19d790a
Diogo  Alberto <106744072+albertokff@users.noreply.github.com>
2024-06-20 22:35:08 -0300

    ✨feat: created account page front-end

--- manipulator/src/pages/account-page.vue
+++ manipulator/src/pages/account-page.vue
@@ -7,14 +7,66 @@
         >
   
           <v-card                   
-            title="Account Page"
-            subtitle="Subtitle"
-            text="..."
+            title="Minha conta"
+            subtitle="Insira e atualize os dados de sua conta"            
             style="background-color: white;color:black;height:90vh"                    
           >
-            <v-card-actions>
-              <v-btn>Click me Aqui!</v-btn>
-            </v-card-actions>
+            <v-container>
+              <v-row>
+                <v-col cols="2">
+                  <v-list-subheader style="color:black;">Nome Completo</v-list-subheader>
+                </v-col>
+
+                <v-col cols="8">
+                  <v-text-field
+                    label="Nome"                                        
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="2">
+                  <v-list-subheader style="color:black;">E-mail</v-list-subheader>
+                </v-col>
+
+                <v-col cols="8">
+                  <v-text-field
+                    label="E-mail"
+                    type="email"                                        
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="2">
+                  <v-list-subheader style="color:black;">Senha</v-list-subheader>
+                </v-col>
+
+                <v-col cols="8">
+                  <v-text-field
+                    label="Senha"                                        
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="2">
+                  <v-list-subheader style="color:black;">Confirmação de senha</v-list-subheader>
+                </v-col>
+
+                <v-col cols="8">
+                  <v-text-field                                                           
+                    label="Confirmação de Senha"
+                  ></v-text-field>
+                </v-col>                
+              </v-row>
+
+              <v-row class="d-flex justify-end pr-10">
+                <v-btn>
+                  Editar
+                </v-btn>
+              </v-row>
+            </v-container>
           </v-card>
                   
           <Footer />
